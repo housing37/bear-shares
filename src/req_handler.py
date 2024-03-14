8641d9a7fc6bc33870b9ecf2a708af673cf8ec06
house <housesellout@gmail.com>
2024-03-14 17:07:34 -0400

    :- req_handler bug fix with running chrome headless on AWS ubuntu instance
    need to install correct version (given in error: 122. or something like that)
        # $ unzip chromedriver_linux64.zip
        # $ cd chromedriver_linux64
        # $ sudo rm /usr/bin/chromedriver
        # $ sudo mv chromedriver /usr/bin
        # $ chromedriver --version
    
    then add option —-no-sandbox
    
    then testing on bot local mac and remote ubuntu instance works fine

--- src/req_handler.py
+++ src/req_handler.py
@@ -512,13 +512,13 @@ def search_tweet_for_text(tweet_url, _lst_text=[], _headless=True):
         options = Options()
         print(f' using --headless={_headless}')
         if _headless:
-            options.add_argument("--headless")  # Run Chrome in headless mode
-
-            # required, else '--headless' fails
             # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
-            user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.111 Safari/537.36"
             # user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"
-            options.add_argument(f"user-agent={user_agent}")
+            user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.111 Safari/537.36" # AWS ubuntu instance
+            
+            options.add_argument("--no-sandbox") # ref: https://stackoverflow.com/a/53073789 (required for AWS ubuntu instance)
+            options.add_argument("--headless")  # Run Chrome in headless mode
+            options.add_argument(f"user-agent={user_agent}") # required, else '--headless' fails
             options.add_argument("--enable-javascript")  
 
         # Initialize a Selenium WebDriver & get tweet_url page
