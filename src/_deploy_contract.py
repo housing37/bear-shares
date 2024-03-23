c55f2ef7507db1033c09a91e39573081ac3550ba
house <housesellout@gmail.com>
2024-03-22 20:33:02 -0400

    :- cleaned up and organize _deploy_contract.py 	- made it more functional instead of open script running 	- added main() function and default support, etc. 	- initial testing looks good

--- src/_deploy_contract.py
+++ src/_deploy_contract.py
@@ -22,28 +22,32 @@ LST_CONTR_ABI_BIN = [
     "../bin/contracts/BearSharesTrinity",
 ]
 
-# init _w3, user select abi to deploy, generate contract & deploy
-_w3 = _web3.myWEB3().init_inp()
-abi_file, bin_file = _w3.inp_sel_abi_bin(LST_CONTR_ABI_BIN) # returns .abi|bin
-_CONTRACT = _w3.add_contract_deploy(abi_file, bin_file)
+W3_ = None
+ABI_FILE = None
+BIN_FILE = None
+CONTRACT = None
 
-print(f'\nDEPLOYING bytecode: {bin_file}')
-print(f'DEPLOYING abi: {abi_file}')
-assert input('\n (1) procced? [y/n]\n  > ') == 'y', "aborted...\n"
+def init_web3():
+    global W3_, ABI_FILE, BIN_FILE, CONTRACT
+    # init W3_, user select abi to deploy, generate contract & deploy
+    W3_ = _web3.myWEB3().init_inp()
+    ABI_FILE, BIN_FILE = W3_.inp_sel_abi_bin(LST_CONTR_ABI_BIN) # returns .abi|bin
+    CONTRACT = W3_.add_contract_deploy(ABI_FILE, BIN_FILE)
 
 def estimate_gas(contract, contract_args=[]):
+    global W3_, ABI_FILE, BIN_FILE, CONTRACT
     # Replace with your contract's ABI and bytecode
     # contract_abi = CONTR_ABI
     # contract_bytecode = CONTR_BYTES
     
     # Replace with your wallet's private key
-    private_key = _w3.SENDER_SECRET
+    private_key = W3_.SENDER_SECRET
 
     # Create a web3.py contract object
-    # contract = _w3.W3.eth.contract(abi=contract_abi, bytecode=contract_bytecode)
+    # contract = W3_.W3.eth.contract(abi=contract_abi, bytecode=contract_bytecode)
 
     # Set the sender's address from the private key
-    sender_address = _w3.W3.eth.account.from_key(private_key).address
+    sender_address = W3_.W3.eth.account.from_key(private_key).address
 
     # Estimate gas for contract deployment
     # gas_estimate = contract.constructor().estimateGas({'from': sender_address})
@@ -52,10 +56,10 @@ def estimate_gas(contract, contract_args=[]):
     print(f"\nEstimated gas cost _ 0: {gas_estimate}")
 
     import statistics
-    block = _w3.W3.eth.get_block("latest", full_transactions=True)
+    block = W3_.W3.eth.get_block("latest", full_transactions=True)
     gas_estimate = int(statistics.median(t.gas for t in block.transactions))
-    gas_price = _w3.W3.eth.gas_price
-    gas_price_eth = _w3.W3.from_wei(gas_price, 'ether')
+    gas_price = W3_.W3.eth.gas_price
+    gas_price_eth = W3_.W3.from_wei(gas_price, 'ether')
     print(f"Estimated gas cost _ 1: {gas_estimate}")
     print(f" Current gas price: {gas_price_eth} ether (PLS) == {gas_price} wei")
     # Optionally, you can also estimate the gas price (in Gwei) using a gas price strategy
@@ -68,12 +72,13 @@ def estimate_gas(contract, contract_args=[]):
 # note: params checked/set in priority order; 'def|max_params' uses 'mpf_ratio'
 #   if all params == False, falls back to 'min_params=True' (ie. just use 'gas_limit')
 def get_gas_params_lst(rpc_url, min_params=False, max_params=False, def_params=True):
+    global W3_, ABI_FILE, BIN_FILE, CONTRACT
     # Estimate the gas cost for the transaction
     #gas_estimate = buy_tx.estimate_gas()
-    gas_limit = _w3.GAS_LIMIT # max gas units to use for tx (required)
-    gas_price = _w3.GAS_PRICE # price to pay for each unit of gas (optional?)
-    max_fee = _w3.MAX_FEE # max fee per gas unit to pay (optional?)
-    max_prior_fee = _w3.MAX_PRIOR_FEE # max fee per gas unit to pay for priority (faster) (optional)
+    gas_limit = W3_.GAS_LIMIT # max gas units to use for tx (required)
+    gas_price = W3_.GAS_PRICE # price to pay for each unit of gas (optional?)
+    max_fee = W3_.MAX_FEE # max fee per gas unit to pay (optional?)
+    max_prior_fee = W3_.MAX_PRIOR_FEE # max fee per gas unit to pay for priority (faster) (optional)
     #max_priority_fee = W3.to_wei('0.000000003', 'ether')
 
     if min_params:
@@ -96,9 +101,76 @@ def generate_contructor():
         constr_args.append(arg)
     return constr_args
 
-def get_time_now(dt=True):
-    if dt: return '['+datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[0:-4]+']'
-    return '['+datetime.now().strftime("%H:%M:%S.%f")[0:-4]+']'
+def main():
+    global W3_, ABI_FILE, BIN_FILE, CONTRACT
+    init_web3()
+    print(f'\nDEPLOYING bytecode: {BIN_FILE}')
+    print(f'DEPLOYING abi: {ABI_FILE}')
+    assert input('\n (1) procced? [y/n]\n  > ') == 'y', "aborted...\n"
+
+    constr_args = generate_contructor() # 0x78b48b71C8BaBd02589e3bAe82238EC78966290c
+    print(f'  using "constructor({", ".join(map(str, constr_args))})"')
+    assert input('\n (2) procced? [y/n]\n  > ') == 'y', "aborted...\n"
+
+    # proceed = estimate_gas(CONTRACT, constr_args) # (3) proceed? [y/n]
+    # assert proceed, "\ndeployment canceled after gas estimate\n"
+
+    print('\ncalculating gas ...')
+    tx_nonce = W3_.W3.eth.get_transaction_count(W3_.SENDER_ADDRESS)
+    tx_params = {
+        'chainId': W3_.CHAIN_ID,
+        'nonce': tx_nonce,
+    }
+    lst_gas_params = get_gas_params_lst(W3_.RPC_URL, min_params=False, max_params=True, def_params=True)
+    for d in lst_gas_params: tx_params.update(d) # append gas params
+
+    print(f'building tx w/ NONCE: {tx_nonce} ...')
+    # constructor_tx = CONTRACT.constructor().build_transaction(tx_params)
+    constructor_tx = CONTRACT.constructor(*constr_args).build_transaction(tx_params)
+
+    print(f'signing and sending tx ... {get_time_now()}')
+    # Sign and send the transaction # Deploy the contract
+    tx_signed = W3_.W3.eth.account.sign_transaction(constructor_tx, private_key=W3_.SENDER_SECRET)
+    tx_hash = W3_.W3.eth.send_raw_transaction(tx_signed.rawTransaction)
+
+    print(cStrDivider_1, f'waiting for receipt ... {get_time_now()}', sep='\n')
+    print(f'    tx_hash: {tx_hash.hex()}')
+
+    # Wait for the transaction to be mined
+    wait_time = 300 # sec
+    try:
+        tx_receipt = W3_.W3.eth.wait_for_transaction_receipt(tx_hash, timeout=wait_time)
+        print("Transaction confirmed in block:", tx_receipt.blockNumber, f' ... {get_time_now()}')
+    # except W3_.W3.exceptions.TransactionNotFound:    
+    #     print(f"Transaction not found within the specified timeout... wait_time: {wait_time}", f' ... {get_time_now()}')
+    # except W3_.W3.exceptions.TimeExhausted:
+    #     print(f"Transaction not confirmed within the specified timeout... wait_time: {wait_time}", f' ... {get_time_now()}')
+    except Exception as e:
+        print(f"\n{get_time_now()}\n Transaction not confirmed within the specified timeout... wait_time: {wait_time}")
+        print_except(e)
+        exit(1)
+
+    # print incoming tx receipt (requires pprint & AttributeDict)
+    tx_receipt = AttributeDict(tx_receipt) # import required
+    tx_rc_print = pprint.PrettyPrinter().pformat(tx_receipt)
+    print(cStrDivider_1, f'RECEIPT:\n {tx_rc_print}', sep='\n')
+    print(cStrDivider_1, f"\n\n Contract deployed at address: {tx_receipt['contractAddress']}\n\n", sep='\n')
+
+#------------------------------------------------------------#
+#   DEFAULT SUPPORT                                          #
+#------------------------------------------------------------#
+READ_ME = f'''
+    *DESCRIPTION*
+        deploy contract to chain
+         selects .abi & .bin from ../bin/contracts/
+
+    *NOTE* INPUT PARAMS...
+        nil
+        
+    *EXAMPLE EXECUTION*
+        $ python3 {__filename} -<nil> <nil>
+        $ python3 {__filename}
+'''
 
 #ref: https://stackoverflow.com/a/1278740/2298002
 def print_except(e, debugLvl=0):
@@ -115,85 +187,18 @@ def print_except(e, debugLvl=0):
         strTrace = traceback.format_exc()
         print('', cStrDivider, f' type: {exc_type}', f' file: {fname}', f' line_no: {exc_tb.tb_lineno}', f' traceback: {strTrace}', cStrDivider, sep='\n')
 
-constr_args = generate_contructor() # 0x78b48b71C8BaBd02589e3bAe82238EC78966290c
-print(f'  using "constructor({", ".join(map(str, constr_args))})"')
-assert input('\n (2) procced? [y/n]\n  > ') == 'y', "aborted...\n"
-
-# proceed = estimate_gas(_CONTRACT, constr_args) # (3) proceed? [y/n]
-# assert proceed, "\ndeployment canceled after gas estimate\n"
-
-print('\ncalculating gas ...')
-tx_nonce = _w3.W3.eth.get_transaction_count(_w3.SENDER_ADDRESS)
-tx_params = {
-    'chainId': _w3.CHAIN_ID,
-    'nonce': tx_nonce,
-}
-lst_gas_params = get_gas_params_lst(_w3.RPC_URL, min_params=False, max_params=True, def_params=True)
-for d in lst_gas_params: tx_params.update(d) # append gas params
-
-print(f'building tx w/ NONCE: {tx_nonce} ...')
-# constructor_tx = _CONTRACT.constructor().build_transaction(tx_params)
-constructor_tx = _CONTRACT.constructor(*constr_args).build_transaction(tx_params)
-
-print(f'signing and sending tx ... {get_time_now()}')
-# Sign and send the transaction # Deploy the contract
-tx_signed = _w3.W3.eth.account.sign_transaction(constructor_tx, private_key=_w3.SENDER_SECRET)
-tx_hash = _w3.W3.eth.send_raw_transaction(tx_signed.rawTransaction)
-
-print(cStrDivider_1, f'waiting for receipt ... {get_time_now()}', sep='\n')
-print(f'    tx_hash: {tx_hash.hex()}')
-
-# Wait for the transaction to be mined
-wait_time = 300 # sec
-try:
-    tx_receipt = _w3.W3.eth.wait_for_transaction_receipt(tx_hash, timeout=wait_time)
-    print("Transaction confirmed in block:", tx_receipt.blockNumber, f' ... {get_time_now()}')
-# except _w3.W3.exceptions.TransactionNotFound:    
-#     print(f"Transaction not found within the specified timeout... wait_time: {wait_time}", f' ... {get_time_now()}')
-# except _w3.W3.exceptions.TimeExhausted:
-#     print(f"Transaction not confirmed within the specified timeout... wait_time: {wait_time}", f' ... {get_time_now()}')
-except Exception as e:
-    print(f"\n{get_time_now()}\n Transaction not confirmed within the specified timeout... wait_time: {wait_time}")
-    print_except(e)
-    exit(1)
-
-# print incoming tx receipt (requires pprint & AttributeDict)
-tx_receipt = AttributeDict(tx_receipt) # import required
-tx_rc_print = pprint.PrettyPrinter().pformat(tx_receipt)
-print(cStrDivider_1, f'RECEIPT:\n {tx_rc_print}', sep='\n')
-print(cStrDivider_1, f"\n\n Contract deployed at address: {tx_receipt['contractAddress']}\n\n", sep='\n')
-
-#------------------------------------------------------------#
-#   DEFAULT SUPPORT                                          #
-#------------------------------------------------------------#
-READ_ME = f'''
-    *DESCRIPTION*
-        deploy a contract with CLI command prompt
-
-    *NOTE* INPUT PARAMS...
-        nil
-        
-    *EXAMPLE EXECUTION*
-        $ python3 {__filename} -<nil> <nil>
-        $ python3 {__filename}
-'''
-#ref: https://stackoverflow.com/a/1278740/2298002
-def print_except(e, debugLvl=0):
-    #print(type(e), e.args, e)
-    print('', cStrDivider, f' Exception Caught _ e: {e}', cStrDivider, sep='\n')
-    if debugLvl > 0:
-        print('', cStrDivider, f' Exception Caught _ type(e): {type(e)}', cStrDivider, sep='\n')
-    if debugLvl > 1:
-        print('', cStrDivider, f' Exception Caught _ e.args: {e.args}', cStrDivider, sep='\n')
-
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    strTrace = traceback.format_exc()
-    print('', cStrDivider, f' type: {exc_type}', f' file: {fname}', f' line_no: {exc_tb.tb_lineno}', f' traceback: {strTrace}', cStrDivider, sep='\n')
+def wait_sleep(wait_sec : int, b_print=True, bp_one_line=True): # sleep 'wait_sec'
+    print(f'waiting... {wait_sec} sec')
+    for s in range(wait_sec, 0, -1):
+        if b_print and bp_one_line: print(wait_sec-s+1, end=' ', flush=True)
+        if b_print and not bp_one_line: print('wait ', s, sep='', end='\n')
+        time.sleep(1)
+    if bp_one_line and b_print: print() # line break if needed
+    print(f'waiting... {wait_sec} sec _ DONE')
 
 def get_time_now(dt=True):
-    if dt: return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[0:-4]
-    return datetime.now().strftime("%H:%M:%S.%f")[0:-4]
+    if dt: return '['+datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[0:-4]+']'
+    return '['+datetime.now().strftime("%H:%M:%S.%f")[0:-4]+']'
 
 def read_cli_args():
     print(f'\nread_cli_args...\n # of args: {len(sys.argv)}\n argv lst: {str(sys.argv)}')
@@ -209,7 +214,7 @@ if __name__ == "__main__":
     
     ## exe ##
     try:
-        pass
+        main()
     except Exception as e:
         print_except(e, debugLvl=0)
     
