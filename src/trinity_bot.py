c5b1a5a908f2aefb2141cbd8cb3f1edc50f3a452
house <housesellout@gmail.com>
2024-04-12 21:49:29 -0400

    … bug fix

--- src/trinity_bot.py
+++ src/trinity_bot.py
@@ -468,7 +468,7 @@ async def cmd_exe(update: Update, context: CallbackContext, aux_cmd=False):
             d_resp_sel['user owed'] = '$' + f"{d_resp['usd_owed']:.2f}"
             d_resp_sel['user paid'] = '$' + f"{d_resp['usd_owed']:.2f}"
             d_resp_sel['user total earned'] = '$' + f"{d_resp['usd_owed']:.2f}"
-            str_r = '\n '.join([str(k)+': '+str(d_resp_sel[k]) for k in d_resp.keys()])
+            str_r = '\n '.join([str(k)+': '+str(d_resp_sel[k]) for k in d_resp_sel.keys()])
             msg_txt = f"Shill has been approved for payment ...\n {str_r}"            
             await update.callback_query.message.reply_text(msg_txt) # reply to message sender
 
