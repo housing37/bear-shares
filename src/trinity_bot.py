380a650f17bdf6498360f23e5d2c30f69d10d648
house <housesellout@gmail.com>
2024-04-12 22:02:01 -0400

    …. tuple fix

--- src/trinity_bot.py
+++ src/trinity_bot.py
@@ -480,9 +480,9 @@ async def cmd_exe(update: Update, context: CallbackContext, aux_cmd=False):
             # await context.bot.send_message(chat_id=user.id, text=msg_txt) # send DM to approved user
             if update.message is None or update.message.chat_id is None or str(update.message.chat_id) != str(CHAT_ID_0): # if reply_text is not to the main chat
                 print(f'CHAT_ID_0: {CHAT_ID_0}')
-                print(f'CHAT_ID_0: {int(CHAT_ID_0)}')
-                print(f'CHAT_ID_0: {str(CHAT_ID_0)}')
-                await context.bot.send_message(chat_id=int(CHAT_ID_0), text=msg_txt) # send to CHAT_ID_0 = "BearShares - trinity"
+                print(f'CHAT_ID_0: {int(CHAT_ID_0[0])}')
+                print(f'CHAT_ID_0: {str(CHAT_ID_0[0])}')
+                await context.bot.send_message(chat_id=int(CHAT_ID_0[0]), text=msg_txt) # send to CHAT_ID_0 = "BearShares - trinity"
 
             # LEFT OFF HERE ... need to store and return chat IDs for TG groups using the bot (and tg_user_id)
 
