13b0b0cb7ada80b6d41cdb26ff07bd6bd30de0a8
house <housesellout@gmail.com>
2024-05-09 04:25:01 -0400

    … refactored some globals to private

--- contracts/FLRBalancer_BEAR9.sol
+++ contracts/FLRBalancer_BEAR9.sol
@@ -12,41 +12,42 @@ import "./pkg-balancer/interfaces/contracts/vault/IVault.sol";
 import "./pkg-balancer/interfaces/contracts/vault/IFlashLoanRecipient.sol";
 
 contract FLRBalancerBEAR9 is IFlashLoanRecipient {
-    // ref: https://docs.balancer.fi/reference/contracts/flash-loans.html#example-code
-    IVault private constant BALANCER_VAULT = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    // pWETH: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 -> 203658647860394116213752201 / 10**18 == 203658647.86039412 ~= $12,228.2445082
-    // pUSDC: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 -> 23937491402753 / 10**6 == 23937491.402753 ~= $74,767.781978741
-    // pWBTC: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599 -> 5994742939 / 10**8 == 59.94742939 ~= $13,208.5705829
-    // pUSDT: 0xdAC17F958D2ee523a2206206994597C13D831ec7 -> 13296690613518 / 10**6 == 13296690.613518 ~= $39,213.741269448
-
-    // pDOLA: 0x865377367054516e17014CcdED1e7d814EDC9ce4 -> 30614508079920854526255527 / 10**18 ~= $236.198849851
-    // pAAVE: 0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9 -> 2114928347848533872595 / 10**18 ~= $280.453860038
-
-    // ERROR: LIQUIDITY < BALANCE
-    // pBAL: 0xba100000625a3754423978a60c9317c58a424e3D ->  3946496821522180948639451 / 10**18 == 3946496.821522181 ~= $41,153.679623988
-    // prETH: 0xae78736cd615f374d3085123a210448e74fc6393 -> 27843230642023975590639 / 10**18 ~= $8,067.713537987
-
-    address public constant BURN_ADDR = address(0x0000000000000000000000000000000000000369);
-    address public constant TOK_WPLS = address(0xA1077a294dDE1B09bB078844df40758a5D0f9a27);
-    
-    address public constant TOK_ATROPA = address(0xCc78A0acDF847A2C1714D2A925bB4477df5d48a6);
-    address public constant TOK_TSFi = address(0x4243568Fa2bbad327ee36e06c16824cAd8B37819);
-    address public constant TOK_R = address(0x557F7e30aA6D909Cfe8a229A4CB178ab186EC622);
-    address public constant TOK_BEAR9 = address(0x1f737F7994811fE994Fe72957C374e5cD5D5418A);
-
-    address public constant TOK_pWBTC = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-    address public constant TOK_pUSDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    address public constant TOK_pWETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    address public constant TOK_pUSDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-    
-    // address private constant LP_WALLET = address(0xEEd80539c314db19360188A66CccAf9caC887b22);
-
     /* -------------------------------------------------------- */
-    /* GLOBALS                                                  */
+    /* PUBLIC - GLOBALS                                          
     /* -------------------------------------------------------- */
     /* _ ADMIN SUPPORT _ */
     string public tVERSION = '0.0';
     address public KEEPER;
+
+    /* -------------------------------------------------------- */
+    /* PRIVATE - GLOBALS                                          
+    /* -------------------------------------------------------- */
+    // ref: https://docs.balancer.fi/reference/contracts/flash-loans.html#example-code
+    IVault private constant BALANCER_VAULT = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+        // pWETH: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 -> 203658647860394116213752201 / 10**18 == 203658647.86039412 ~= $12,228.2445082
+        // pUSDC: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 -> 23937491402753 / 10**6 == 23937491.402753 ~= $74,767.781978741
+        // pWBTC: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599 -> 5994742939 / 10**8 == 59.94742939 ~= $13,208.5705829
+        // pUSDT: 0xdAC17F958D2ee523a2206206994597C13D831ec7 -> 13296690613518 / 10**6 == 13296690.613518 ~= $39,213.741269448
+
+        // pDOLA: 0x865377367054516e17014CcdED1e7d814EDC9ce4 -> 30614508079920854526255527 / 10**18 ~= $236.198849851
+        // pAAVE: 0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9 -> 2114928347848533872595 / 10**18 ~= $280.453860038
+
+        // ERROR: LIQUIDITY < BALANCE
+        // pBAL: 0xba100000625a3754423978a60c9317c58a424e3D ->  3946496821522180948639451 / 10**18 == 3946496.821522181 ~= $41,153.679623988
+        // prETH: 0xae78736cd615f374d3085123a210448e74fc6393 -> 27843230642023975590639 / 10**18 ~= $8,067.713537987
+
+    address private constant BURN_ADDR = address(0x0000000000000000000000000000000000000369);
+    address private constant TOK_WPLS = address(0xA1077a294dDE1B09bB078844df40758a5D0f9a27);
+    
+    address private constant TOK_ATROPA = address(0xCc78A0acDF847A2C1714D2A925bB4477df5d48a6);
+    address private constant TOK_TSFi = address(0x4243568Fa2bbad327ee36e06c16824cAd8B37819);
+    address private constant TOK_R = address(0x557F7e30aA6D909Cfe8a229A4CB178ab186EC622);
+    address private constant TOK_BEAR9 = address(0x1f737F7994811fE994Fe72957C374e5cD5D5418A);
+
+    address private constant TOK_pWBTC = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+    address private constant TOK_pUSDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address private constant TOK_pWETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address private constant TOK_pUSDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
     /* -------------------------------------------------------- */
     /* STRUCTS                                        
@@ -62,6 +63,9 @@ contract FLRBalancerBEAR9 is IFlashLoanRecipient {
     event FlashLoansReceived(IERC20[] _tokens, uint256[] _amounts, uint256[] _feeAmounts, bytes _userData);
     event FlashLoansReturned(IERC20[] _tokens, uint256[] _amounts, uint256[] _feeAmounts, uint256[] _amountsOwed);
 
+    /* -------------------------------------------------------- */
+    /* CONTRUCTOR                                        
+    /* -------------------------------------------------------- */
     constructor() {
         KEEPER = msg.sender;
     }
