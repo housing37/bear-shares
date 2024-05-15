995c4cafa7ba6d5d480dcbc54eb872df8ae089b7
house <housesellout@gmail.com>
2024-05-15 18:17:37 -0400

    :- *** INIT UPDATE / integration … UniswapFlashQuer.sol *** 	- updated ‘getPairsByIndexRange’ 		now returns another array with reserves and ‘amountIn’ for all those pairs 	- added whiteboard screen shot of 2 addition algorithms and equations to use ‘next’
    NEXT:
    	i have now come up with 2 new algorithms to extract value from LPs
    		1) kill 3 liquidity pools algorithm
    		2) balance 3 liquidity pools algorithm
    	i have also figured out 2 equations for these algorithms for integration 
    	i have also updated getPairsByIndexRange to return reserves and additional info for algorithm #2
    	next: need to design and integrate python side… 
    		- need to traverse the results from ‘getPairsByIndexRange’
    		- then compare and organize 2 lists of LPs and their tokens
    			1 list for each algorithm/equation (added .jpg) that LPs are found to conform to

--- contracts/UniswapFlashQuery.sol
+++ contracts/UniswapFlashQuery.sol
@@ -1,7 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.12;
+pragma solidity >=0.6.12;
+// pragma solidity ^0.8.24;
 
 pragma experimental ABIEncoderV2;
+
+import "./node_modules/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 interface IUniswapV2Pair {
     function token0() external view returns (address);
@@ -17,6 +20,7 @@ abstract contract UniswapV2Factory  {
 
 // In order to quickly load up data from Uniswap-like market, this contract allows easy iteration with a single eth_call
 contract FlashBotsUniswapQuery {
+    string public constant tVERSION = '0.1';
     function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
         uint256[3][] memory result = new uint256[3][](_pairs.length);
         for (uint i = 0; i < _pairs.length; i++) {
@@ -25,7 +29,8 @@ contract FlashBotsUniswapQuery {
         return result;
     }
 
-    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
+    // function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
+    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, address _uniswapRouter, uint256 _start, uint256 _stop) external view returns (address[3][] memory, uint256[5][] memory)  {
         uint256 _allPairsLength = _uniswapFactory.allPairsLength();
         if (_stop > _allPairsLength) {
             _stop = _allPairsLength;
@@ -33,12 +38,33 @@ contract FlashBotsUniswapQuery {
         require(_stop >= _start, "start cannot be higher than stop");
         uint256 _qty = _stop - _start;
         address[3][] memory result = new address[3][](_qty);
+        uint256[5][] memory result_res = new uint256[5][](_qty);
         for (uint i = 0; i < _qty; i++) {
             IUniswapV2Pair _uniswapPair = IUniswapV2Pair(_uniswapFactory.allPairs(_start + i));
-            result[i][0] = _uniswapPair.token0();
-            result[i][1] = _uniswapPair.token1();
-            result[i][2] = address(_uniswapPair);
+            // result[i][0] = _uniswapPair.token0();
+            // result[i][1] = _uniswapPair.token1();
+            // result[i][2] = address(_uniswapPair);
+            result[i][0] = address(_uniswapPair);
+            result[i][1] = _uniswapPair.token0();
+            result[i][2] = _uniswapPair.token1();
+
+            // (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)
+            (result_res[i][0], result_res[i][1], result_res[i][4]) = _uniswapPair.getReserves();
+
+            result_res[i][2] = IUniswapV2Router02(_uniswapRouter).getAmountIn(result_res[i][0], result_res[i][1], result_res[i][0]);
+            result_res[i][3] = IUniswapV2Router02(_uniswapRouter).getAmountIn(result_res[i][1], result_res[i][0], result_res[i][1]);
+
+            // address[] memory path_1_0 = new address[](2);
+            // path_1_0[0] = _uniswapPair.token1();
+            // path_1_0[1] = _uniswapPair.token0();
+            // result_res[i][2] = IUniswapV2Router02(_uniswapRouter).getAmountsIn(result_res[i][0], path_1_0)[0];
+            
+            // address[] memory path_0_1 = new address[](2);
+            // path_0_1[0] = _uniswapPair.token0();
+            // path_0_1[1] = _uniswapPair.token1();
+            // result_res[i][3] = IUniswapV2Router02(_uniswapRouter).getAmountsIn(result_res[i][1], path_0_1)[0];
+            // UniswapV2Library.getAmountsOut(factory, amountIn, path);
         }
-        return result;
+        return (result, result_res);
     }
 }
